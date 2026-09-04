70d809c7bd44d7fc0f73235e9ed6a96711a5c12c
ionch <konglongtan@gmail.com>
2026-09-04 14:30:22 +0800

    Fourth? commit!!!!!

--- hello.py
+++ hello.py
@@ -1,21 +1,18 @@
-print("==============")
-print("Welcome here")
-print("My first post!")
-print("==============")
+# print("==============")
+# print("Welcome here")
+# print("My first post!")
+# print("==============")
 
-username = "High Tier Human"
-bio = "What is a LTG to a HTH"
-followers = 100
+# username = "High Tier Human"
+# bio = "What is a LTG to a HTH"
+# followers = 100
 
-print("Username:" , username)
-print("Bio:", bio)
-print("Followers:", followers)
+username = input("Enter Username: ")
+age = input("Enter age: ")
+category = input("Enter Content Category: ")
 
-followers += 50
-print("Day 1: ", followers)
-
-followers += 20
-print("Day 2: ", followers)
-
-followers -= 10
-print("Day 3: ", followers)
+print("\nInstagram Profile")
+print("=================")
+print("Username: ", username)
+print("Age: ", age)
+print("Category: ", category)
