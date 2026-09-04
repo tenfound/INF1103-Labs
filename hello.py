558c0ff60eb1c55d0bca717ecf3c5e851e8abdd1
ionch <konglongtan@gmail.com>
2026-09-04 14:43:02 +0800

    submission

--- hello.py
+++ hello.py
@@ -7,6 +7,17 @@
 # bio = "What is a LTG to a HTH"
 # followers = 100
 
+# print("Username: ", username)
+# print("Bio: ", bio)
+# print("Followers: ", followers)
+
+# followers += 50
+# print("Day 1: ", followers)
+# followers += 20
+# print("Day 2: ", followers)
+# followers -= 10
+# print("Day 3: ", followers)
+
 username = input("Enter Username: ")
 age = input("Enter age: ")
 category = input("Enter Content Category: ")
