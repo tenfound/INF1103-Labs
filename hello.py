9e0fbcbe478ade3608ae100a699356878c582a76
ionch <konglongtan@gmail.com>
2026-09-04 14:25:11 +0800

    Second commit!!!!

--- hello.py
+++ hello.py
@@ -2,3 +2,11 @@ print("==============")
 print("Welcome here")
 print("My first post!")
 print("==============")
+
+username = "High Tier Human"
+bio = "What is a LTG to a HTH"
+followers = 100
+
+print("Username:" , username)
+print("Bio:", bio)
+print("Followers:", followers)
