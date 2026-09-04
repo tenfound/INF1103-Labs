d18df2b3148bfab7fd7a643e4d325a31ef101e52
ionch <konglongtan@gmail.com>
2026-09-04 14:40:42 +0800

    Completed lab 1

--- hello.py
+++ hello.py
@@ -16,3 +16,29 @@ print("=================")
 print("Username: ", username)
 print("Age: ", age)
 print("Category: ", category)
+
+if age > 40 and category == "fun":
+    print("You are old and what is fun for you??") 
+
+# A1: 
+# a) Program displays messages as written
+# b) Top to bottom
+# c) Output appears in terminal
+
+# A2:
+# a) Variables allow us to store the value we want in them, allowing us to "pull" them for use later on
+# b) Yes output changes if I change the values
+
+# A3:
+# a) No, we are only updating the value of followers using the operatives
+# b) It affects the latest/existing value of followers
+# c) It adds or subtracts from the variable
+
+# A4:
+# a) It allows the user to enter the input from within the terminal
+# b) The program is now dynamic
+# c) Value changes according to what is inputted
+
+# A5: 
+# a) Return type for input is a string
+# b) Conditions are checked using the if statement. If both conditions are met, then the code within the statement will run
